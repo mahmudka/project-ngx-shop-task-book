55b685ffa3e85177dc3fa10a95bf135215f06bf5
mahmudka <mahmudka@mail.ru>
2020-09-20 13:58:09 +0300

    Third Not finished

---
+++ 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'ngx-shop-star-rating',
+  templateUrl: './star-rating.component.html',
+})
+export class StarRatingComponent {
+  @Input()
+  //  @ts-ignore
+  public feedbackRate = 0;
+  public stars = [0, 1, 2, 3, 4];
+
+  public highlight(index: number): boolean {
+    return (index + 1) <= Math.round(this.feedbackRate);
+  }
+}
