260c4df73bdd5affcb8ef000feb316d79a6eec27
Anna Varaksina <enigmasnape@gmail.com>
2022-02-23 01:16:05 +0300

    feat(study): Интерполяция, связывание данных и межкомпонентная коммуникаци

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,8 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Output, Input, EventEmitter } from '@angular/core';
+import { ICartProduct } from 'shared/mocks/1-components/cart-product';
 
 @Component({
 	selector: 'ngx-shop-card',
 	templateUrl: './shop-card.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShopCardComponent {}
+export class ShopCardComponent {
+	@Input()
+	public product: ICartProduct = {} as ICartProduct;
+
+	@Output()
+	public increment: EventEmitter<number> = new EventEmitter<number>();
+	
+	@Output()
+	public decrement: EventEmitter<number> = new EventEmitter<number>();
+
+	public incrementProductInCart(): void {
+		this.increment.emit();
+	}
+	public decrementProductInCart(): void {
+		this.decrement.emit();
+	}
+
+	public getSum() {
+		return this.product?.price * this.product?.count;
+	}
+}
