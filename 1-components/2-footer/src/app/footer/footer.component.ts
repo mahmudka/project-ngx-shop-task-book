4994c0b203a11d941f31cde458dbc1dffa381cb3
Anna Varaksina <enigmasnape@gmail.com>
2022-02-23 21:33:49 +0300

    feat(study): Интерполяция, связывание данных и межкомпонентная коммуникаци

--- 1-components/2-footer/src/app/footer/footer.component.ts
+++ 1-components/2-footer/src/app/footer/footer.component.ts
@@ -7,6 +7,6 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 })
 export class FooterComponent {
 	public author = 'Angular Course';
-	
+
 	public currentYear: number = new Date().getFullYear();
 }
--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,8 +1,29 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { IProduct } from 'shared/mocks/1-components/product';
 
 @Component({
 	selector: 'ngx-shop-content-product',
 	templateUrl: './product-card.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CategoryProductComponent {}
+export class CategoryProductComponent {
+
+	@Input()
+	public product: IProduct = {} as IProduct;
+
+	@Output()
+	public addToCart: EventEmitter<string> = new EventEmitter<string>();
+
+	@Output()
+	// public goToProduct: EventEmitter<string> = new EventEmitter<string>();
+	public goToProduct: EventEmitter<string> = new EventEmitter<string>();
+
+	public addToBasket():void {
+		this.addToCart.emit('Добавлено в корзину');
+	};
+
+	public redirectTo():void {
+		this.goToProduct.emit('Редирект');
+	};
+	
+}
