6ef2610858eee6ca8527a863605f7d654eefab9a
Anna Varaksina <enigmasnape@gmail.com>
2022-03-23 01:25:15 +0300

    feat(study): Директивы. Создание компонента меню

--- 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
+++ 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
@@ -1,8 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICategory } from 'shared/mocks/3-directives/categories';
 
 @Component({
 	selector: 'ngx-shop-side-menu',
 	templateUrl: './side-menu.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+	@Input()
+	public categories: ICategory[] = [];
+
+	@Output()
+	public categoryChange: EventEmitter<string> = new EventEmitter<string>();
+
+	public currentName: string | null = null;
+
+	public hover(name: string | undefined) {
+		if (!name) {
+			return;
+		}
+		this.currentName = name;
+	}
+
+	public mouseLeave() {
+		this.currentName = null;
+	}
+
+	public redirectTo(subCategory: string | undefined): void {
+		this.categoryChange.emit(subCategory);
+	}
+}
